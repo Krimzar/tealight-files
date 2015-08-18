64d15d87dc2c72c17938f0002c5fe69f53af2269
Krimzar <lukeharrison59@gmail.com>
2015-08-18 14:47:33 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -9,11 +9,25 @@ from tealight.robot import (move,
 
 
 thing = look() #Initialise
-while thing == "fruit":
-  move()
-  thing = look()
-  
+count = smell()
 
+
+def IfFood(Side):
+  if Side == "left":
+    turn(3)
+  elif Side == "right":
+    turn(1)
+  move()
+
+while count > 0: 
+  if touch() == "fruit": 
+    IfFood("front")
+  elif left_side() == "fruit":
+    IfFood("left")
+  elif right_side == "fruit":
+    IfFood("right")
+  
+  
 
 
 
