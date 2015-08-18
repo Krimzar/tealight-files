a07f9d17fb8319f463f03c000a96e26a7ebfe0ec
Krimzar <lukeharrison59@gmail.com>
2015-08-18 12:29:52 +0100

    Closing logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -16,18 +16,16 @@ def DrawChessboard(length):
 
 
     
-for i in range(0,8):
-  if i % 2 != 0:
-    DrawChessboard(8)
-    turn(180)
-  else:
-    DrawChessboard(8)
-    turn(-90)
+
+DrawChessboard(8)
+turn(180)
+DrawChessboard(8)
+turn(-90)
 move(80)
 turn(-90)
 move(320)
 turn(-180)
-#DrawChessboard(8)
+DrawChessboard(8)
 
 
 #angle = 360 / edges
