510fff175cacd91e40449dae09c69907e8d10954
Krimzar <lukeharrison59@gmail.com>
2015-08-18 15:17:41 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -12,8 +12,6 @@ thing = touch() #Initialise
 count = smell() 
 path = 0
 
-global path
-
 
 #def TurnRobot(IsFruit, path):
   
@@ -28,22 +26,19 @@ global path
     
 
 def MoveRobot(): 
+  while smell() > 0:
+    if left_side() == "fruit":
+      turn(3)
+    elif right_side() == "fruit":
+      turn(1)
+    move()
+    path += 1
   
-  if left_side() == "fruit":
-    turn(3)
-  elif right_side() == "fruit":
-    turn(1)
-  move()
-  
-  path += 1
-  
-  if smell() > 0: 
-    MoveRobot()
   else: 
     for i in range(path):
       turn(-2)
       move()
-      path -= 1
+    path = 0 
   MoveRobot()
   
 MoveRobot()
