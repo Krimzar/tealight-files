beeb411c1919c7137f559e9da3f96611becab164
Krimzar <lukeharrison59@gmail.com>
2015-08-18 12:14:37 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -6,7 +6,7 @@ def polygon(edges, size):
     move(size)
     turn(angle)
  
-def DrawChessboard(length, width):
+def DrawChessboard(width):
   for i in range(0, width):
       
     polygon(4,40)
@@ -15,7 +15,7 @@ def DrawChessboard(length, width):
 
       
 for i in range(0,2):
-  DrawChessboard(8,8)   
+  DrawChessboard(8)   
 
 #angle = 360 / edges
 #  decoration = size / 2
