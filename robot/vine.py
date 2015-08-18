1687e1b94225444bb4e64cd0c1320a4a87410a79
Krimzar <lukeharrison59@gmail.com>
2015-08-18 14:59:08 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -7,3 +7,19 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here
+
+thing = touch()
+count = smell() #Initialise
+
+while count > 0:
+  if touch() == "fruit":
+    move()
+  elif left_side() == "fruit":
+    turn(3)
+  elif right_side() == "fruit":
+    turn(1)
+  if touch() == "fruit":
+    pass
+  else:
+    move()
+  
