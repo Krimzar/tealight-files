8ffbeaa8ea347d4a1ea609f0193f664abdeaee89
Krimzar <lukeharrison59@gmail.com>
2015-08-20 14:07:29 +0100

    Running art/racecar.py

--- art/racecar.py
+++ art/racecar.py
@@ -27,7 +27,7 @@ CoordCentre["y"] = screen_height / 2
 
 CoordA = {"x": (CoordCentre["x"]-15), "y": (CoordCentre["y"]+15)}   
 CoordB = {"x": (CoordCentre["x"]-15), "y": (CoordCentre["y"]-15)} 
-CoordC = {"x": (CoordCentre["x"]+30), "y": (CoordCentre["y"]+ 0.01)}                                              
+CoordC = {"x": (CoordCentre["x"]+30), "y": (CoordCentre["y"]+ 1)}                                              
 CoordD = {"x": (CoordCentre["x"]), "y": (CoordCentre["y"])} 
 #Initialise car coordinates
 #AD BD CD
