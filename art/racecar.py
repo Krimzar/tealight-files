999dfa93cbd3afa369d47fd855a7acac8e52bfeb
Krimzar <lukeharrison59@gmail.com>
2015-08-20 10:41:01 +0100

    Running art/racecar.py

--- art/racecar.py
+++ art/racecar.py
@@ -8,17 +8,16 @@ print screen_height
 CoordCentre = {"x": 0, "y": 0} #Initialise coordinates of car centre
 
 CoordCentre["x"] = screen_width / 2
-CoordCenter["y"] = screen_height / 2
+CoordCentre["y"] = screen_height / 2
 
 
 
 color("black")
 
-CoordA = {"x": CoordCenter["x"], "y": CoordCenter["y"]}   
-CoordB = {"x": CoordCenter["x"], "y": (CoordCenter["y"]-50)} 
-CoordC = {"x": (CoordCenter["x"]+100), "y": (CoordCenter["y"]-25)} 
-                                             
-CoordD = {"x": (CoordCenter["x"]+50), "y": (CoordCenter["y"]-25)} 
+CoordA = {"x": CoordCentre["x"], "y": CoordCentre["y"]}   
+CoordB = {"x": CoordCentre["x"], "y": (CoordCentre["y"]-50)} 
+CoordC = {"x": (CoordCentre["x"]+100), "y": (CoordCentre["y"]-25)}                                              
+CoordD = {"x": (CoordCentre["x"]+50), "y": (CoordCentre["y"]-25)} 
 
 #del point["#"] to delete a point
 
