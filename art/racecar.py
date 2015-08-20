07c90a9e211dd81e53f10065ed2b5c2f2ca18e24
Krimzar <lukeharrison59@gmail.com>
2015-08-20 10:28:19 +0100

    Running art/racecar.py

--- art/racecar.py
+++ art/racecar.py
@@ -2,6 +2,9 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 
 from tealight.art import screen_width, screen_height
 
+color(00CC00)
+
+
 CoordA = {"x": 0, "y": 10}   #Initialise dictionary of car coordinates
 CoordB = {"x": 0, "y": 5}
 CoordC = {"x": 10, "y": 7.5}
