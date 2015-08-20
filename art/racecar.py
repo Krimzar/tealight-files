b48974507c84341874b67d8713324a5f0158edd8
Krimzar <lukeharrison59@gmail.com>
2015-08-20 10:40:14 +0100

    Running art/racecar.py

--- art/racecar.py
+++ art/racecar.py
@@ -5,39 +5,21 @@ from tealight.art import screen_width, screen_height
 print screen_width
 print screen_height
 
+CoordCentre = {"x": 0, "y": 0} #Initialise coordinates of car centre
+
+CoordCentre["x"] = screen_width / 2
+CoordCenter["y"] = screen_height / 2
 
 
-color("#00CC00")
 
-CoordA = {"x": 328, "y": 322}   #Initialise dictionary of car coordinates
-CoordB = {"x": 328, "y": 317}
-CoordC = {"x": 338, "y": 319.5}
-CoordD = {"x": 333, "y": 319.5}
+color("black")
+
+CoordA = {"x": CoordCenter["x"], "y": CoordCenter["y"]}   
+CoordB = {"x": CoordCenter["x"], "y": (CoordCenter["y"]-50)} 
+CoordC = {"x": (CoordCenter["x"]+100), "y": (CoordCenter["y"]-25} 
+CoordD = {"x": (CoordCenter["x"]+50), "y": (CoordCenter["y"]-25)} 
 
 #del point["#"] to delete a point
 
 line(CoordA["x"], CoordA["y"], CoordB["x"], CoordA["y"])
 
-lastx = 0
-lasty = 0
-
-def handle_mousedown(x,y):
-  global lastx, lasty
- 
-  lastx = x
-  lasty = y
- 
-def handle_mousemove(x,y,button):
-  global lastx, lasty
-  print (lastx)
-  print (lasty)
-  if button == "left":
-    color("black")
-    line(lastx, lasty, x, y)
-    lastx = x
-    lasty = y
-  elif button == "right":
-    color("red")
-    line(lastx, lasty, x, y)
-    lastx = x
-    lasty = y
