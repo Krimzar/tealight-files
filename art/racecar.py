6cfd03f157353b7fbb45c18539fd83a5743b6dfa
Krimzar <lukeharrison59@gmail.com>
2015-08-20 10:27:50 +0100

    Running art/racecar.py

--- art/racecar.py
+++ art/racecar.py
@@ -1,1 +1,12 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+from tealight.art import screen_width, screen_height
+
+CoordA = {"x": 0, "y": 10}   #Initialise dictionary of car coordinates
+CoordB = {"x": 0, "y": 5}
+CoordC = {"x": 10, "y": 7.5}
+CoordD = {"x": 5, "y": 7.5}
+
+#del point["#"] to delete a point
+
+line(CoordA["x"], CoordA["y"], CoordB["x"], CoordA["y"])
