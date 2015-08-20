e23ba9fedf62030a48bd7ca904af52b2d7a5d57c
Krimzar <lukeharrison59@gmail.com>
2015-08-20 12:07:23 +0100

    Running art/racecar.py

--- art/racecar.py
+++ art/racecar.py
@@ -40,7 +40,7 @@ def draw_car():
 def handle_keydown(key): 
   global CoordA, CoordB, CoordC, CoordD
   if key == "down": 
-    CoordC["y"] -= 50
+    CoordC["y"] += 50
     
 def handle_frame():
   draw_car()
