2875f62e4b9770d0dd53590f9972f14c44778b8c
Krimzar <lukeharrison59@gmail.com>
2015-08-20 14:54:57 +0100

    Running art/racecar.py

--- art/racecar.py
+++ art/racecar.py
@@ -7,147 +7,122 @@ from math import sin, cos, tan, pi, sqrt, acos, atan, asin, radians, degrees
 print screen_width
 print screen_height
 
-CoordCentre = {"x": 0, "y": 0} #Initialise coordinates of car centre
-TotalOrientation = 0.0
-Acceleration = 0.0 #Initialisation of oritenation and acceleration as floats
-Speed = 0.0 
-Friction = 0.003
+car car1 = None
 
-AngleA = 0.0
-AngleB = 0.0
-AngleC = 0.0
-DistAD = 0.0
-DistBD = 0.0
-DistCD = 0.0
+class car:
 
+  CoordCentre = {"x": 0, "y": 0} #Initialise coordinates of car centre
+  TotalOrientation = 0.0 #Initialisation of total orientation moved by the car
+  Acceleration = 0.0 #Initialisation of acceleration as a float
+  Speed = 0.0 #Initialisation of speed as a float
+  Friction = 0.003 #Initialisation of friction constant
+  
+  AngleA = 0.0 #Initialises angles and distances needed for trigonometric calculcations
+  AngleB = 0.0
+  AngleC = 0.0
+  DistAD = 0.0
+  DistBD = 0.0
+  DistCD = 0.0
+  
+  
+  CoordCentre["x"] = screen_width / 2 #Determines centre of the screen as (x,y)
+      
+  CoordCentre["y"] = screen_height / 2
+  
 
-CoordCentre["x"] = screen_width / 2
+  CoordA = {"x": (CoordCentre["x"]-15), "y": (CoordCentre["y"]+15)}   
+  CoordB = {"x": (CoordCentre["x"]-15), "y": (CoordCentre["y"]-15)} 
+  CoordC = {"x": (CoordCentre["x"]+30), "y": (CoordCentre["y"])}                                              
+  CoordD = {"x": (CoordCentre["x"]), "y": (CoordCentre["y"])} 
+  #Initialise car coordinates
+  
+  
+  DistAD = sqrt((((CoordA["x"] - CoordD["x"])**2) + ((CoordA["y"] - CoordD["y"])**2)))
+  DistBD = sqrt((((CoordB["x"] - CoordD["x"])**2) + ((CoordB["y"] - CoordD["y"])**2)))
+  DistCD = CoordD["x"] - CoordC["x"] #Trigonometry to calculate orientation of racecar
+  
+  AngleA = atan((CoordD["x"] - CoordA["x"]) / (CoordD["y"] - CoordA["y"]))
+  AngleB = atan((CoordD["x"] - CoordB["x"]) / (CoordD["y"] - CoordB["y"]))
+  
+  def draw_car(self):
     
-CoordCentre["y"] = screen_height / 2
-
-#del point["#"] to delete a point
-
-CoordA = {"x": (CoordCentre["x"]-15), "y": (CoordCentre["y"]+15)}   
-CoordB = {"x": (CoordCentre["x"]-15), "y": (CoordCentre["y"]-15)} 
-CoordC = {"x": (CoordCentre["x"]+30), "y": (CoordCentre["y"])}                                              
-CoordD = {"x": (CoordCentre["x"]), "y": (CoordCentre["y"])} 
-#Initialise car coordinates
-#AD BD CD
-
-DistAD = sqrt((((CoordA["x"] - CoordD["x"])**2) + ((CoordA["y"] - CoordD["y"])**2)))
-DistBD = sqrt((((CoordB["x"] - CoordD["x"])**2) + ((CoordB["y"] - CoordD["y"])**2)))
-DistCD = CoordD["x"] - CoordC["x"]
-
-AngleA = atan((CoordD["x"] - CoordA["x"]) / (CoordD["y"] - CoordA["y"]))
-AngleB = atan((CoordD["x"] - CoordB["x"]) / (CoordD["y"] - CoordB["y"]))
-
-def draw_car():
-  global CoordA, CoordB, CoordC, CoordD, CoordCentre
+    color("red")
+      
+    line(self.CoordA["x"], self.CoordA["y"], self.CoordB["x"], self.CoordB["y"])
+    line(self.CoordA["x"], self.CoordA["y"], self.CoordD["x"], self.CoordD["y"])
+    line(self.CoordB["x"], self.CoordB["y"], self.CoordD["x"], self.CoordD["y"])
+    line(self.CoordA["x"], self.CoordA["y"], self.CoordC["x"], self.CoordC["y"])
+    line(self.CoordB["x"], self.CoordB["y"], self.CoordC["x"], self.CoordC["y"])
+    line(self.CoordD["x"], self.CoordD["y"], self.CoordC["x"], self.CoordC["y"])
+    #Initialise lines of car
   
-  color("red")
     
-  line(CoordA["x"], CoordA["y"], CoordB["x"], CoordB["y"])
-  line(CoordA["x"], CoordA["y"], CoordD["x"], CoordD["y"])
-  line(CoordB["x"], CoordB["y"], CoordD["x"], CoordD["y"])
-  line(CoordA["x"], CoordA["y"], CoordC["x"], CoordC["y"])
-  line(CoordB["x"], CoordB["y"], CoordC["x"], CoordC["y"])
-  line(CoordD["x"], CoordD["y"], CoordC["x"], CoordC["y"])
-  #Initialise lines of car
-
+  def change_orientation(self, Orientation): 
+    
+    self.TotalOrientation += self.Orientation
+    
+    self.AngleA += radians(self.Orientation)
+    self.AngleB += radians(self.Orientation)
+    self.AngleC += radians(self.Orientation) 
+    
+    self.CoordA["x"] = ((self.DistAD * sin(self.AngleA)) + self.CoordD["x"])
+    self.CoordA["y"] = ((self.DistAD * cos(self.AngleA)) + self.CoordD["y"])
+    
+    self.CoordB["x"] = ((self.DistBD * sin(self.AngleB)) + self.CoordD["x"])
+    self.CoordB["y"] = ((self.DistBD * cos(self.AngleB)) + self.CoordD["y"])
+    
+    self.CoordC["x"] = ((self.DistCD * sin(self.AngleC)) + self.CoordD["x"])
+    self.CoordC["y"] = ((self.DistCD * cos(self.AngleC)) + self.CoordD["y"])
   
-def change_orientation(Orientation): 
+  def update_speed(self):
+    if self.Speed > 1.0:
+      self.Speed = 1.0
+    else:
+      self.Speed += self.Acceleration
+    if self.Acceleration  > 0:
+      self.Acceleration -= (self.Friction * self.Speed)
+    
+    
+    self.ChangeY = -cos(radians(self.TotalOrientation)) * self.Speed
+    self.ChangeX = -sin(radians(self.TotalOrientation)) * self.Speed
+    
+    self.CoordA["x"] += self.ChangeX
+    self.CoordA["y"] += self.ChangeY
+    
+    self.CoordB["x"] += self.ChangeX
+    self.CoordB["y"] += self.ChangeY
+    
+    self.CoordC["x"] += self.ChangeX
+    self.CoordC["y"] += self.ChangeY
+    
+    self.CoordD["x"] += self.ChangeX
+    self.CoordD["y"] += self.ChangeY
+    
+    
   
-  global AngleA, AngleB, AngleC, DistAD, DistBD, DistCD, TotalOrientation
-  
-  TotalOrientation += Orientation
-  
-  AngleA += radians(Orientation)
-  AngleB += radians(Orientation)
-  AngleC += radians(Orientation) 
-  
-  CoordA["x"] = ((DistAD * sin(AngleA)) + CoordD["x"])
-  CoordA["y"] = ((DistAD * cos(AngleA)) + CoordD["y"])
-  
-  CoordB["x"] = ((DistBD * sin(AngleB)) + CoordD["x"])
-  CoordB["y"] = ((DistBD * cos(AngleB)) + CoordD["y"])
-  
-  CoordC["x"] = ((DistCD * sin(AngleC)) + CoordD["x"])
-  CoordC["y"] = ((DistCD * cos(AngleC)) + CoordD["y"])
-
-def update_speed():
-  global Speed, TotalOrientation, Acceleration
-  if Speed > 1.0:
-    Speed = 1.0
-  else:
-    Speed += Acceleration
-  if Acceleration  > 0:
-    Acceleration -= (Friction * Speed)
-  
-  
-  ChangeY = -cos(radians(TotalOrientation)) * Speed
-  ChangeX = -sin(radians(TotalOrientation)) * Speed
-  
-  CoordA["x"] += ChangeX
-  CoordA["y"] += ChangeY
-  
-  CoordB["x"] += ChangeX
-  CoordB["y"] += ChangeY
-  
-  CoordC["x"] += ChangeX
-  CoordC["y"] += ChangeY
-  
-  CoordD["x"] += ChangeX
-  CoordD["y"] += ChangeY
-  
+    
+def start():
+  car1 = car()
   
 def handle_keydown(key): 
-  global Angle, Acceleration
-  if key == "left":   
-    change_orientation(5)
-  elif key == "right":
-    change_orientation(-5)
-  elif key == "up": 
-    if Acceleration > 0.05: 
-      Acceleration = 0.05
-    else:
-      Acceleration += 0.01
-    
-    
-    
-    
-
-    
-    
-    
+    if key == "left":   
+      car1.change_orientation(5)
+    elif key == "right":
+      car1.change_orientation(-5)
+    elif key == "up": 
+      if car1.Acceleration > 0.05: 
+        car1.Acceleration = 0.05
+      else:
+        car1.Acceleration += 0.01
+      
 def handle_frame():
-  update_speed()
-  color("white")
-  box(0, 0, 10000, 10000)
-  draw_car()
+    car1.update_speed()
+    color("white")
+    box(0, 0, 10000, 10000)
+    car1.draw_car()
   
-    
-    
- 
 
+start()
 
-
-
-#def star(x, y, c, size, spines):
-  
-#  color(c)
-  
- # angle = 0
-  
-#  for i in range(0, spines):
-  #  x0 = x + (60 * cos(angle))
- #   y0 = y + (size * sin(angle))
-    
- #   line(x, y, x0, y0)
-    
- #   angle = angle + (2 * pi / spines)
-
-#star(300, 300, "blue", 100, 50)
-#star(600, 400, "purple", 200, 100)
-#star(450, 200, "orange", 125, 30)
 
 
