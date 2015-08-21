2657ea00ba32845ca9e7bfb27b90cb7486013336
Krimzar <lukeharrison59@gmail.com>
2015-08-21 09:27:32 +0100

    Running art/racecar.py

--- art/racecar.py
+++ art/racecar.py
@@ -59,7 +59,7 @@ class car:
       color("blue")
     
     #color("black")
-    text(self.CoordC["x"]-10, self.CoordC["y"], "Foo")
+    text(self.CoordA["x"]-10, self.CoordA["y"], "Foo")
  
     line(self.CoordA["x"], self.CoordA["y"], self.CoordB["x"], self.CoordB["y"])
     line(self.CoordA["x"], self.CoordA["y"], self.CoordD["x"], self.CoordD["y"])
